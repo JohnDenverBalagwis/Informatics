34202315f1ccb3680af77815659a17959d376e54
GitActivity-MonroyoNeilAndrei <andreimonroyo0@gmail.com>
2024-02-14 15:43:09 +0800

    adding of candidates

---
+++ classes/database.php
@@ -0,0 +1,444 @@
+<?php
+class database
+{
+  private $servername = 'localhost';
+  private $username = 'root';
+  private $password = '';
+  private $dbname = 'pageant_db';
+  public $last_id;
+  public $last_ballot;
+  public $result = array();
+  public $mysqli = '';
+
+  public function __construct()
+  {
+    $this->mysqli = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+  }
+
+
+
+  public function pullLastRowModified($table, $column)
+  {
+    $sql = "SELECT * FROM $table WHERE id = $this->last_id";
+    $result = $this->mysqli->query($sql);
+
+    $row = $result->fetch_assoc();
+    return $row[$column];
+  }
+
+  public function isExisted($table, $para = array())
+  {
+    $args = array();
+    $where = '';
+
+    foreach ($para as $key => $value) {
+
+      $args[] = "$key = '$value'";
+
+      $where = implode(' && ', $args);
+    }
+
+    $select = " SELECT * FROM $table WHERE $where";
+    $check = $this->mysqli->query($select);
+
+    // gets the id of the  selected row
+    while ($row = $check->fetch_assoc()) {
+      $this->last_ballot = $this->last_id = $row['id'];
+    }
+
+    if (!$check) {
+      die('Invalid query: ' . $this->mysqli->error);
+    } else if (mysqli_num_rows($check) > 0) {
+      // there is a data in the table
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public function insertData($table, $para = array())
+  {
+    $table_columns = implode(',', array_keys($para));
+    $table_value = implode("','", $para);
+
+    $insert = "INSERT INTO $table($table_columns)  VALUES('$table_value')";
+
+    if ($this->mysqli->query($insert) === TRUE) {
+      $this->last_id = $this->mysqli->insert_id;
+    } else {
+      die('Invalid query: ' . $this->mysqli->error);
+    }
+  }
+
+  // public function updateData($table, $para = array(), $column, $value_id)
+  // {
+  //   $args = array();
+
+  //   foreach ($para as $key => $value) {
+  //     $args[] = "$key = '$value'";
+  //   }
+
+  //   $sql = "UPDATE  $table SET " . implode(',', $args);
+
+  //   if(is_int($value)) {
+  //     $sql .= " WHERE $column = $value_id";
+  //   }else{
+  //     $sql .= " WHERE $column = '$value_id'";
+
+  //   }
+  //   $update = $this->mysqli->query($sql);
+  //   $this->last_id = $value_id;
+
+
+  // }
+
+  public function checkIfImage($name)
+  {
+    $img_name = $_FILES["$name"]['name'];
+    $img_size = $_FILES["$name"]['size'];
+    $tmp_name = $_FILES["$name"]['tmp_name'];
+    $error = $_FILES["$name"]['error'];
+
+    if ($error === 0) {
+      $img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
+      $img_ex_lc = strtolower($img_ex);
+
+      $allowed_exs = array('jpg', 'jpeg', 'png');
+
+      if (in_array($img_ex_lc, $allowed_exs)) {
+
+        return true;
+      }else {
+        return false;
+      }
+
+      
+    }
+  }
+
+  public function insertImage($name, $table, $column, $path)
+  {
+    $img_name = $_FILES["$name"]['name'];
+    $img_size = $_FILES["$name"]['size'];
+    $tmp_name = $_FILES["$name"]['tmp_name'];
+    $error = $_FILES["$name"]['error'];
+
+
+
+    if ($error === 0) {
+      $img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
+      $img_ex_lc = strtolower($img_ex);
+
+      $allowed_exs = array('jpg', 'jpeg', 'png');
+
+      if (in_array($img_ex_lc, $allowed_exs)) {
+        $new_img_name = uniqid("IMG-", true) . '.' . $img_ex_lc;
+
+        $img_upload_path = $path . $new_img_name;
+
+        move_uploaded_file($tmp_name, $img_upload_path);
+
+        $update = "UPDATE $table SET $column = '$new_img_name' WHERE id = $this->last_id";
+        return $this->mysqli->query($update);
+      }else {
+        return false;
+      }
+
+      
+    }
+  }
+
+  public function checkIfPDF($name)
+  {
+    $img_name = $_FILES["$name"]['name'];
+    $img_size = $_FILES["$name"]['size'];
+    $tmp_name = $_FILES["$name"]['tmp_name'];
+    $error = $_FILES["$name"]['error'];
+
+
+
+    if ($error === 0) {
+      $img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
+      $img_ex_lc = strtolower($img_ex);
+
+      $allowed_exs = array('pdf');
+
+      if (in_array($img_ex_lc, $allowed_exs)) {
+        return true;
+      }else {
+        return false;
+      }
+
+      
+    }
+  }
+
+
+  public function insertPDF($name, $table, $column, $path)
+  {
+    $img_name = $_FILES["$name"]['name'];
+    $img_size = $_FILES["$name"]['size'];
+    $tmp_name = $_FILES["$name"]['tmp_name'];
+    $error = $_FILES["$name"]['error'];
+
+
+
+    if ($error === 0) {
+      $img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
+      $img_ex_lc = strtolower($img_ex);
+
+      $allowed_exs = array('pdf');
+
+      if (in_array($img_ex_lc, $allowed_exs)) {
+        $new_img_name = uniqid("IMG-", true) . '.' . $img_ex_lc;
+
+        $img_upload_path = $path . $new_img_name;
+
+        move_uploaded_file($tmp_name, $img_upload_path);
+
+        $update = "UPDATE $table SET $column = '$new_img_name' WHERE id = $this->last_id";
+        $this->mysqli->query($update);
+        return true;
+      }else {
+        return false;
+      }
+
+      
+    }
+  }
+
+  public function updateImage($name, $table, $column, $path)
+  {
+    $img_name = $_FILES["$name"]['name'];
+    $img_size = $_FILES["$name"]['size'];
+    $tmp_name = $_FILES["$name"]['tmp_name'];
+    $error = $_FILES["$name"]['error'];
+
+
+
+    if ($error === 0) {
+      $img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
+      $img_ex_lc = strtolower($img_ex);
+
+      $allowed_exs = array('jpg', 'jpeg', 'png');
+
+      if (in_array($img_ex_lc, $allowed_exs)) {
+
+        $result = $this->select($table, '*', ['id' => $this->last_id]);
+        $row = mysqli_fetch_assoc($result);
+        echo $path . $row["$column"];
+        unlink("$path" . $row["$column"]);
+        
+
+        $new_img_name = uniqid("IMG-", true) . '.' . $img_ex_lc;
+
+        $img_upload_path = "$path" . $new_img_name;
+
+        move_uploaded_file($tmp_name, $img_upload_path);
+
+        $update = "UPDATE $table SET $column = '$new_img_name' WHERE id = $this->last_id";
+        return $this->mysqli->query($update);
+      }else {
+        return false;
+      }
+    }
+  }
+
+  public function deleteImage ($photo_url, $path) {
+    if(isset($_GET["$photo_url"])){
+      unlink($path . $_GET["$photo_url"]);
+    }
+  }
+
+  // public function select($table, $rows = '*', $column = null, $value = null)
+  // {
+  //   if ($column != null && $value != null) {
+  //     if (is_int($value)) {
+  //     $sql = "SELECT $rows FROM $table WHERE $column = $value";
+  //     } else {
+  //       $sql = "SELECT $rows FROM $table WHERE $column = '$value'";
+  //     }
+  //   } else {
+  //     $sql = "SELECT $rows FROM $table";
+  //   }
+
+  //   return $this->mysqli->query($sql);
+  // }
+
+
+  public function delete($table, $id)
+  {
+
+    $sql = "DELETE FROM $table";
+    $sql .= " WHERE $id ";
+    return $this->mysqli->query($sql);
+  }
+
+  public function __destruct()
+  {
+    $this->mysqli->close();
+  }
+
+  public function deleteRow($table, $where = "") {
+
+    $sql = "DELETE FROM $table WHERE $where";
+
+    return $this->mysqli->query($sql);
+  }
+
+
+  public function updateData($table, $para = array(), $where = array(), $operator = 'AND')
+  {
+    $args = array();
+
+    foreach ($para as $key => $value) {
+      $args[] = "$key = '$value'";
+    }
+
+    $where_args = array();
+    foreach ($where as $key => $value) {
+      $where_args[] = "$key = '$value'";
+    }
+
+    $sql = "UPDATE  $table SET " . implode(', ', $args);
+
+    if (!empty($where)) {
+      $sql .= " WHERE " . implode(" $operator ", $where_args);
+    }
+
+    $this->last_id = $value;
+    return $this->mysqli->query($sql);
+  }
+
+  public function advanceUpdateData($table, $para = array(), $where = '')
+  {
+    $args = array();
+
+    foreach ($para as $key => $value) {
+      $args[] = "$key = '$value'";
+    }
+
+    $sql = "UPDATE  $table SET " . implode(', ', $args);
+
+    if ($where != '') {
+      $sql .= " WHERE $where";
+    }
+
+    $this->last_id = $value;
+    return $this->mysqli->query($sql);
+  }
+
+
+
+  public function incrementData($table, $column, $where = array(), $operator = 'AND') {
+
+    $where_args = array();
+    foreach($where as $key => $value) {
+      $where_args[] = "$key = $value";
+      $this->last_id = $value;
+    }
+
+    $sql = "UPDATE $table SET $column = $column + 1";
+
+    if (!empty($where)) {
+      $sql .= " WHERE " . implode(" $operator ", $where_args);
+    }
+    
+    return $this->mysqli->query($sql);
+
+  }
+
+  public function select($table, $rows = '*', $where = array(), $operator = 'AND')
+  {
+    if (empty($where)) {
+      $sql = "SELECT $rows FROM $table";
+      return $this->mysqli->query($sql);
+    }
+
+    $args = array();
+
+    foreach ($where as $key => $value) {
+      if (is_int($value)) {
+        $args[] = "$key = $value";
+      } else {
+        $args[] = "$key = '$value'";
+      }
+    }
+
+    $sql = "SELECT $rows FROM $table WHERE " . implode(" $operator ", $args);
+    return $this->mysqli->query($sql);
+  }
+
+  public function limitSelectAll($table, $limit = 0, $offset = 0)
+  {
+    if($limit == 0 && $offset == 0){
+      $sql = "SELECT * FROM $table ORDER BY id DESC";
+      return $this->mysqli->query($sql);
+    }
+    $sql = "SELECT * FROM $table ORDER BY id DESC LIMIT $limit OFFSET $offset";
+    return $this->mysqli->query($sql);
+  }
+
+  public function selectDistinct($table, $rows = '*', $where = array(), $operator = 'AND', $order = "")
+  {
+    if (empty($where)) {
+      $sql = "SELECT DISTINCT $rows FROM $table";
+      return $this->mysqli->query($sql);
+    }
+
+    $args = array();
+
+    foreach ($where as $key => $value) {
+      if (is_int($value)) {
+        $args[] = "$key = $value";
+      } else {
+        $args[] = "$key = '$value'";
+      }
+    }
+
+
+    $sql = "SELECT DISTINCT $rows FROM $table WHERE " . implode(" $operator ", $args);
+    return $this->mysqli->query($sql);
+    
+  }
+
+  public function advanceSelectDistinct($table, $rows = '*', $where = '')
+  {
+    if ($where == '') {
+      $sql = "SELECT DISTINCT $rows FROM $table";
+      return $this->mysqli->query($sql);
+    } else if($where != '') {
+      $sql = "SELECT DISTINCT $rows FROM $table WHERE $where";
+      return $this->mysqli->query($sql);
+    }
+  }
+
+
+  public function advanceSelect($table, $rows = '*', $where = '')
+  {
+    if ($where == '') {
+      $sql = "SELECT $rows FROM $table";
+      return $this->mysqli->query($sql);
+    } else if($where != '') {
+      $sql = "SELECT $rows FROM $table WHERE $where";
+      return $this->mysqli->query($sql);
+    }
+  }
+
+  public function countSelect($table, $row = '*', $where = "") {
+    $sql = "SELECT COUNT($row) AS num FROM $table WHERE $where";
+    $result = $this->mysqli->query($sql);
+    $row = mysqli_fetch_assoc($result);
+    return $row['num'];
+  }
+
+  public function search($table, $column1, $column2, $value){
+    $sql = "SELECT * FROM $table WHERE $column1 LIKE '%$value%' OR $column2 LIKE '%$value%';";   
+    return $this->mysqli->query($sql);
+  }
+
+  public function modifiedSearch($table, $where, $column1, $value){
+    $sql = "SELECT * FROM $table WHERE ($where) AND $column1 LIKE '%$value%';";   
+    return $this->mysqli->query($sql);
+  }
+}
--- index.php
+++ index.php
@@ -1,3 +1,24 @@
+<?php 
+include "classes/database.php";
+
+$judges = new database();
+
+if (isset($_POST['submit'])) {
+  $name = mysqli_escape_string($judges->mysqli, $_POST['name']);
+  $age = mysqli_escape_string($judges->mysqli, $_POST['age']);
+  $course = mysqli_escape_string($judges->mysqli, $_POST['course']);
+
+  if ($judges->isExisted('candidates', ['name'=>$name])) {
+    $candidateExist = "Candidate Already Exist";
+  } else {
+    $judges->insertData('candidates', ['name'=>$name, 'age'=>$age, 'course'=>$course]);
+  }
+}
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,7 +26,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://kit.fontawesome.com/5ad1518180.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?<?php echo time(); ?>">
     <title>List of Candidates</title>
 
 </head>
@@ -51,7 +72,23 @@
             <button id="myBtn" class="add-candidate-button">
                 <i class="fas fa-user-plus"></i> Add
             </button>
-        </div>
+
+
+        <?php
+          $candidates = $judges->select('candidates', '*');
+
+          while ($row = mysqli_fetch_assoc($candidates)) {
+
+        ?>
+        <button id="myBtn" class="add-candidate-button">
+            <?php echo $row['name'] . " - " . $row['age'] . " - " . $row['course']; ?>
+        </button>
+        
+        <?php
+          }
+        ?>
+        
+      </div>
 
     </div>
 
@@ -68,15 +105,15 @@
                 </div>
                 <div class="modal-inputs">
                     <label for="">Name:</label>
-                    <input type="text">
+                    <input type="text" name="name" required>
                 </div>
                 <div class="modal-inputs">
                     <label for="">Age:</label>
-                    <input type="text">
+                    <input type="number" name="age" required>
                 </div>
                 <div class="modal-inputs">
                     <label for="">Course:</label>
-                    <input type="text">
+                    <input type="text" name="course" required>
                 </div>
                 <div class="modal-inputs">
                     <label >Upload Photo</label>
@@ -85,7 +122,7 @@
     
                 <div class="modal-buttons">
                     <span id="cancel">Cancel</span>
-                    <a href="">Submit</a>
+                    <input type="submit" name="submit" value="Submit">
                 </div>
             </form >
         </div>
