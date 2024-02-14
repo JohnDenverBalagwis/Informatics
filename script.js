577314ae18ab6dc240423258751e54b5fb7f1bbe
GitActivity-MonroyoNeilAndrei <andreimonroyo0@gmail.com>
2024-02-14 14:40:09 +0800

    modal added

--- script.js
+++ script.js
@@ -36,3 +36,36 @@ function myFunction() {
       }
     }
   }
+
+
+  // Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+const btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+const cancel = document.getElementById("cancel");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+cancel.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
