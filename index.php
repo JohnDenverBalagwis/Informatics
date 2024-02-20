97fb880b8d18e3a8484cee5d1ae0e8dd2961b669
BalagwisJohnDenver <imbajhey@gmail.com>
2024-02-20 09:30:05 +0800

    landing page done

--- index.php
+++ index.php
@@ -7,8 +7,6 @@
     <script src="https://kit.fontawesome.com/5ad1518180.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/cover-page.css">
     <link rel="shortcut icon" href="images/infor.png" type="image/x-icon">
-    <link rel="stylesheet" href="css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Informatics</title>
 </head>
 
@@ -21,27 +19,20 @@
             <div class="name">
                 <div class="informatics">Informatics</div>
                 <div class="college">COLLEGE</div>
+                <a href="candidate-pictures.php" class="get-started-btn">Get Started</a>
             </div>
         </nav>
         <div class="hero-container">
-
             <div class="hero-wrapper">
                 <div class="images-wrapper">
                     <img id="ms" src="images/Mr. & Ms. (1).png" alt="hero-image">
                 </div>
-                
                 <div class="main-content">
                     <h3 class="search">Search for </h3>
                     <h2 class="mrandms">Mr & Ms</h2>
                     <h1 class="icon">Icon</h1>
-
-                    <input class="form-control" type="text" style="position: relative;  top: 2rem; z-index: 4; user-select: auto;"> 
-
                 </div>
-
-
-
-                <a href="candidate-pictures.php" class="get-started-btn">Enter as <br> andrei</a>
+                <input type="text" placeholder="Username" id="Username"><br><br>
 
             </div>
         </div>
